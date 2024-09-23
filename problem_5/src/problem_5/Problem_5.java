4fc5f3a06d1529dfdd3e31810565a71f5ac1b911
ahmedbrol <ahmdbrl811@gmail.com>
2024-09-24 00:40:15 +0300

    problem five

---
+++ problem_5/src/problem_5/Problem_5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problem_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author computop
+ */
+public class Problem_5 {
+
+    static Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+        Double r=input.nextDouble();
+        System.out.println((r*r*3.141592653));
+        
+    }
+    
+}
