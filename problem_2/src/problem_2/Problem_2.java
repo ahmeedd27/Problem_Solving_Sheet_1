65b1a69d2d79a52f0da6329e1a5a5a7482b7bb9e
ahmedbrol <ahmdbrl811@gmail.com>
2024-09-24 00:12:24 +0300

    problem two

---
+++ problem_2/src/problem_2/Problem_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problem_2;
+
+import java.util.Scanner;
+
+
+public class Problem_2 {
+
+  static  Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+         
+      int i=input.nextInt();
+      long l=input.nextLong();
+      char c=input.next().charAt(0);
+      float f=input.nextFloat();
+      double d=input.nextDouble();
+      
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        
+        
+    }
+    
+}
