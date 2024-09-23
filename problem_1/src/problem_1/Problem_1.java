1f9a9a4dc0844397eea5a880e4c0c51725b37599
ahmedbrol <ahmdbrl811@gmail.com>
2024-09-24 00:11:19 +0300

    problem one

---
+++ problem_1/src/problem_1/Problem_1.java
@@ -0,0 +1,27 @@
+
+package problem_1;
+
+import java.util.Scanner;
+
+
+public class Problem_1 {
+
+  static  Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+         
+      int i=input.nextInt();
+      long l=input.nextLong();
+      char c=input.next().charAt(0);
+      float f=input.nextFloat();
+      double d=input.nextDouble();
+      
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        
+        
+    }
+    
+}
