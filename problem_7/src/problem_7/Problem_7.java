938f2af6f11ff9628fdc0b7ac6238e6f40f9a928
ahmeedd27 <ahmdbrl811@gmail.com>
2024-09-26 04:53:44 +0300

    problem seven

---
+++ problem_7/src/problem_7/Problem_7.java
@@ -0,0 +1,17 @@
+
+package problem_7;
+
+import java.util.Scanner;
+
+
+public class Problem_7 {
+
+   static Scanner n=new Scanner(System.in);
+    public static void main(String[] args) {
+      long N=n.nextLong();
+      System.out.println(N*(N+1)/2);
+        
+      }
+
+    
+}
